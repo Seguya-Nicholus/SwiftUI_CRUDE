dc05c0da756852c844d1c7ef7dd5b2d201d15f49
Seguya Nicholus <seguyanicholus2015@gmail.com>
2022-02-17 16:20:03 +0300

    added functionality to view all users

--- SwiftUI_CRUDE/SwiftUI_CRUDE/ContentView.swift
+++ SwiftUI_CRUDE/SwiftUI_CRUDE/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var usersArray: [User] = []
+    
     var body: some View {
         
         // create navigation view
@@ -24,7 +27,26 @@ struct ContentView: View {
                             Text("Add User")
                         })
                 }
-            }
+                
+                //  create listview to show all users
+                List(self.usersArray) { (model) in
+                    
+                    // show name, email and age horizontally
+                    HStack {
+                        Text(model.name)
+                        Spacer()
+                        Text(model.email)
+                        Spacer()
+                        Text("\(model.age)")
+                        
+                    }
+                }
+            }.padding()
+                
+                // load data in the users array
+                    .onAppear(perform: {
+                        self.usersArray = DbUtils().getUsers()
+                    })
         }
     }
 }
